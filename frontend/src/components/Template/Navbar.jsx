b461411a9cff9e25d133fc9cbf9c5aff151f4968
HP <chandan.workholics@gmail.com>
2024-01-09 15:38:23 +0530

    updated

--- frontend/src/components/Template/Navbar.jsx
+++ frontend/src/components/Template/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
                 <div className="container ">
                     <nav className="navbar navbar-expand-sm">
                         <div className="container-fluid">
-                            <Link className="navbar-brand" to="#">Logo</Link>
+                            <Link className="navbar-brand" to="#">Dairy</Link>
                             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
                                 <span className="navbar-toggler-icon"></span>
                             </button>
@@ -37,42 +37,6 @@ const Navbar = () => {
                     </nav>
                 </div>
             </div>
-
-            {/* <nav className="navbar navbar-expand px-3 py-0 border-bottom shadow">
-                <div className="container-fluid">
-                    <Link to="/" className='nav-brand me-5'>
-                        <h2>Logo</h2>
-                    </Link>
-                    <button className='nav-togle-btn hide-mobile' id='sidebar-toggle' type='button' onClick={toggler}>
-                        <span className=""><i class="fa-solid fa-bars-staggered"></i></span>
-                    </button>
-                    <div className="navbar-collapse navbar py-0">
-                        <ul className="navbar-nav align-items-center">
-                            <li className="nav-item">
-                                <form class="d-flex position-relative" role="search">
-                                    <input class="form-control me-2 nav-searchbar rounded-pill" type="search" placeholder="Search here..." aria-label="Search" />
-                                    <button class="btn search-icon" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
-                                </form>
-                            </li>
-                            <li className="nav-item dropdown">
-                                <Link to="/" data-bs-toggle="dropdown" className='nav-icon pe-md-0 nav-link'>
-                                    <img src={require("../img/profile-img.jpg")} className='avatar profile img-fluid rounded' alt="" />
-                                </Link>
-                                <div className="dropdown-menu dropdown-menu-end">
-                                    <Link to="/" className='dropdown-item'>Profile</Link>
-                                    <Link to="/" className='dropdown-item'>Setting</Link>
-                                    <Link to="/" className='dropdown-item'>LogOut</Link>
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
-                    <button className='nav-togle-btn hide-desktop' id='sidebar-toggle' type='button' onClick={toggler}>
-                        <span className=""><i class="fa-solid fa-bars-staggered"></i></span>
-                    </button>
-                </div>
-            </nav> */}
-
-
         </>
     )
 }
